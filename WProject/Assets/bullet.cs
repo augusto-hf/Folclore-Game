f9054abebcc6bf45442a1d51d4a72c79ef4746bf
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-08-31 11:54:44 -0300

    atualização no tiro
    tiro agr colide e é destruido, mas suas particulas ainda estão fora do lugar

---
+++ WProject/Assets/bullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position.normalized, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
