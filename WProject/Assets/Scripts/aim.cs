461d6536c28376663054a327d05a71e53651ef71
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-08-31 11:16:32 -0300

    Tiro
    Asset temporário pra tiro e toda a programação de mira e tiro funcionais

--- WProject/Assets/Scripts/aim.cs
+++ WProject/Assets/Scripts/aim.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mirar : MonoBehaviour
+public class aim : MonoBehaviour
 {
     // Start is called before the first frame update
     public Camera cam;
---
+++ WProject/Assets/Scripts/shooting.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        //spawna o tiro
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position,firePoint.rotation);
+        //chama o Rigid Body 2D da bala
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        //e usa o RB2D pra adicionar a força e acelerar a bala
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
