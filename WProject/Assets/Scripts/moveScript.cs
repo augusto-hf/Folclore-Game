92f1d6729426747b169b57aef5e66606f47f402a
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-09-07 08:13:08 -0300

    Dash adicionado

--- WProject/Assets/Scripts/moveScript.cs
+++ WProject/Assets/Scripts/moveScript.cs
@@ -3,34 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moveScript : MonoBehaviour
-{
+{   //general variables
     public float moveSpeed = 5f;
+    private float startMoveSpeed;
     public Rigidbody2D rb;
     public Animator anim;
     Vector2 movement;
 
+    //dash variables
+    public float dashSpeed;
+    public float dashEnd;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+    private bool isDashing;
+    Vector2 movementRef;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        startMoveSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         move();
+        dash();
         Debug.Log("H: " + movement.x);
         Debug.Log("Y: " + movement.y);
 
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.sqrMagnitude);
+        if (isDashing == false)
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Speed", movement.sqrMagnitude);
+        }
     }
     void FixedUpdate()
-    {
-        //movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
+    {       
+        if (isDashing == false)//movimento sem dash
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else//movimento com dash
+        {
+            rb.MovePosition(rb.position + movementRef * moveSpeed * Time.fixedDeltaTime);
+        }
     }
     void move()
     {
@@ -45,16 +64,42 @@ public class moveScript : MonoBehaviour
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical") + 0.45f;
         }
-        else if (Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical") !=0)
+        else if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {//se o movimento do teclado acontecer mas n for diagonal
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
         }
-        else if (Input.GetAxisRaw("HorizontalC") != 0 || Input.GetAxisRaw("VerticalC") !=0 && Input.GetAxisRaw("Horizontal") == 0 || Input.GetAxisRaw("Vertical") == 0)
+        else if (Input.GetAxisRaw("HorizontalC") != 0 || Input.GetAxisRaw("VerticalC") != 0 && Input.GetAxisRaw("Horizontal") == 0 || Input.GetAxisRaw("Vertical") == 0)
         {//se o movimento do teclado for 0 e o movimento do controle existir de alguma forma
             movement.x = Input.GetAxisRaw("HorizontalC");
             movement.y = Input.GetAxisRaw("VerticalC");
         }
-
+    }
+    void dash()
+    {   //recebe o comando de dar dash caso as variaveis do dash estejam zeradas
+        if (Input.GetButtonDown("Dash") && dashTime <= 0 && moveSpeed == startMoveSpeed)
+        {//da o dash
+            movementRef = movement;
+            isDashing = true;
+            moveSpeed = moveSpeed * dashSpeed;
+            dashTime = startDashTime;
+        }
+        else
+        {//reseta tudo relacionado ao dash         
+            if (dashTime > 0 || moveSpeed > startMoveSpeed)
+            {
+                dashTime -= Time.deltaTime;
+                //O MODIFICADOR AO LADO DO DELTATIME DEFINE A DISTANCIA DO DASH!!! NÃO ESQUEÇA!!!
+                moveSpeed -= Time.deltaTime + dashEnd;
+                //contra medida para bugs:
+                if (moveSpeed < startMoveSpeed)
+                {
+                    moveSpeed = startMoveSpeed;
+                }
+            }
+        }
+        if (dashTime <= 0 && moveSpeed == startMoveSpeed) {//reseta indicador de dash
+        isDashing = false;
+        }
     }
 }
