2bd7892ef53857207ec3fbef61b653af3a3f97cc
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-09-12 21:09:58 -0300

    Shield/Bala
    o tiro agora tem tempo de vida  ajustavel e o escudo esta funcional

--- WProject/Assets/bullet.cs
+++ WProject/Assets/bullet.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-
+    public float bulletLifetime;
     public GameObject hitEffect;
 
+    private void Update()
+    {
+        bulletLifetime -= Time.deltaTime;
+        if (bulletLifetime < 0)
+        {
+            Destroy(gameObject);
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 5f);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Destroy(gameObject);
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
-        Destroy(gameObject);
+        
     }
 }
