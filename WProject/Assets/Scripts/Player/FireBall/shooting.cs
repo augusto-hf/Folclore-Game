f9f31e7492110febe93a0bcfa751d69073dc2c48
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-09-16 04:33:37 -0300

    Super Update
    Mudei os controles novamente, mas dessa vez sem problemas
    Adicionei o furacão perfeitamente funcional
    Agora tem escolha de habilidade entre escudo (1) e tornado (2)

--- WProject/Assets/Scripts/Player/FireBall/shooting.cs
+++ WProject/Assets/Scripts/Player/FireBall/shooting.cs
@@ -6,30 +6,88 @@ using UnityEngine.UI;
 
 public class shooting : MonoBehaviour
 {
+    public tornado tornado;
     public Transform firePoint;
-    public GameObject bulletPrefab;
-    public Text cdText;
+    public GameObject bulletPrefab, Shield, skillTornado;
+    private Rigidbody2D rbT;
     public float bulletForce = 10f;
-    public float attackSpeed = 2f;
-    private float nextAttack = 0f;
+    public float attackSpeed = 2f, skillSpeed = 8f, tornadoForce = 0.6f;
+    private float nextAttack = 0f, nextSkill = 0f, tornadoTimer;
+    private int currentLoadout;
+    public bool isTornadoGoing = false;
 
     internal bool _PlayerStardDialogue;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire") && _PlayerStardDialogue == false && Time.time > nextAttack)
+        loadoutSelect();
+        Shoot();
+        if (currentLoadout == 1)
         {
-            Shoot();
+            shield();
+        }
+        else if (currentLoadout == 2) {
+            tornadoShoot();
+        }
+        if (isTornadoGoing)
+        {
+            tornadoBrake();
+        }
+    }
+    void loadoutSelect()
+    {
+        if (Input.GetButtonDown("Power Select 1"))
+        {
+            currentLoadout = 1;
+            Debug.Log("Power 1 Selected");
+        }
+        if (Input.GetButtonDown("Power Select 2"))
+        {
+            currentLoadout = 2;
+            Debug.Log("Power 2 Selected");
         }
     }
     void Shoot()
     {
+    if (Input.GetButtonDown("Fire") && _PlayerStardDialogue == false && Time.time > nextAttack)
+    {
         //spawna o tiro
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position,firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         //chama o Rigid Body 2D da bala
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         //e usa o RB2D pra adicionar a força e acelerar a bala
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
         nextAttack = Time.time + attackSpeed;
+    }
+    }
+    void shield()
+    {
+        if (Input.GetButtonDown("Power"))
+        {
+            Shield.SetActive(true);
+        }
+        if (Input.GetButtonUp("Power"))
+        {
+            Shield.SetActive(false);
+        }
+    }
+    void tornadoShoot()
+    {
+        if (Input.GetButtonDown("Power") && _PlayerStardDialogue == false && Time.time > nextSkill)
+        {
+            isTornadoGoing = true;
+            GameObject tornado = Instantiate(skillTornado, firePoint.position, Quaternion.identity);
+            rbT = tornado.GetComponent<Rigidbody2D>();
+            rbT.AddForce(firePoint.up * tornadoForce, ForceMode2D.Impulse);
+            nextSkill = Time.time + skillSpeed;
+        }
+    }
+    void tornadoBrake()
+    {
+        if (tornado.tornadoLifeTime == tornado.tornadoLifeTime / 2)
+        {
+            rbT.velocity = Vector2.zero;
+            isTornadoGoing = false;
+        }
     }
 }
--- WProject/Assets/Scripts/Player/Tornado/tornado.cs
+++ WProject/Assets/Scripts/Player/Tornado/tornado.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class tornado : MonoBehaviour
 {
-    private float transitionState;
-    public Transform target;
-    private float initialD;
+    shooting shooting;
+    private GameObject enemyObject;
+    private float distance, pullForce;
+    public float minimumDistance = 2f;
+    public float pullForceReference = 1f, pullForceMultplier;
+    public float tornadoLifeTime = 6f;
 
     void Start()
     {
-        initialD = Vector2.Distance(transform.position, target.position);
+        enemyObject = GameObject.FindWithTag("Enemy");
+        pullForce = pullForceReference;
+        tornadoLifeTime = 5f;
     }
-
     void Update()
     {
-        transitionState += Time.deltaTime;
-        if (transitionState > 1F)
+        tornadoMove();
+        tornadoPullForce();
+        tornadoVaccun();
+        tornadoLifeCounter();
+    }
+    void tornadoLifeCounter()
+    {
+        tornadoLifeTime -= Time.deltaTime;
+        if (tornadoLifeTime < 0)
         {
-            //Being added to inventory code
-            return;
+            Destroy(gameObject);
         }
-        float tdiff = initialD * transitionState;
-        float ad = Vector2.Distance(transform.position, target.position);
-        if (ad > tdiff)
-        {
-            Vector2 npos = Vector2.MoveTowards(transform.position, target.position, ad - tdiff);
-            transform.position = npos;
-        }
+    }
+    void tornadoMove()
+    {
 
+    }
+    void tornadoPullForce()
+    {
+        if (pullForce <= pullForceReference * 2)
+        {//aumenta o pullforce, mas não pode ser mais doque o dobro da original
+            pullForce += Time.deltaTime + pullForce;
+        }
+    }
+    void tornadoVaccun()
+    {
+        distance = Vector2.Distance(enemyObject.transform.position, transform.position);
+        if (distance <= minimumDistance)
+        {
+            enemyObject.GetComponent<Rigidbody2D>().AddForce((transform.position - enemyObject.transform.position) * pullForce);
+        }
     }
 }
