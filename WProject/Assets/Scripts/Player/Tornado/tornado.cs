6809e75130069c6a8660e20fb611b638954fdc3d
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-09-15 23:36:22 -0300

    tentativa de fazer o tornado

--- WProject/Assets/Scripts/Player/Tornado/tornado.cs
+++ WProject/Assets/Scripts/Player/Tornado/tornado.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class tornado : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float transitionState;
+    public Transform target;
+    private float initialD;
+
     void Start()
     {
-        
+        initialD = Vector2.Distance(transform.position, target.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transitionState += Time.deltaTime;
+        if (transitionState > 1F)
+        {
+            //Being added to inventory code
+            return;
+        }
+        float tdiff = initialD * transitionState;
+        float ad = Vector2.Distance(transform.position, target.position);
+        if (ad > tdiff)
+        {
+            Vector2 npos = Vector2.MoveTowards(transform.position, target.position, ad - tdiff);
+            transform.position = npos;
+        }
+
     }
 }
