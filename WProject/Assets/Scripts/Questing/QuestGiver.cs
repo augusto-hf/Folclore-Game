6e40cfedda435c43a11c6a720fcc50d712327e8f
KevinMaia <kevinwmmaia@gmail.com>
2021-09-25 12:40:00 -0300

    Quests
    12:40 pausa pro almoço.

---
+++ WProject/Assets/Scripts/Questing/QuestGiver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public Player player;
+
+    public GameObject questWindow;
+    public Text titleText;
+    public Text descriptionText;
+    public Text itemText;
+
+    public void OpenQuestWindow()
+    {
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+        itemText.text = quest.itemName;
+    }
+}
