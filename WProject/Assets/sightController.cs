c764708545df66c04f14ca72d5695b3677d9d8a2
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-09-27 10:14:04 -0300

    Atualização na luz
    30min trabalhando nisso antes da aula

--- WProject/Assets/sightController.cs
+++ WProject/Assets/sightController.cs
@@ -7,13 +7,21 @@ public class sightController : MonoBehaviour
 {
     public Light2D sight;
     public Player playerScript;
-    public float sightPercent;
+    public float sightPercent, outerRadiusBase;
+    private float maxBlindness, currentBlindness;
 
+    void Start()
+    {
+        //converte pra uma variavel local, ja que a original é int
+        maxBlindness = playerScript.maxBlindness;
+    }
     void Update()
     {
+        //converte pra uma variavel local, ja que a original é int
+        currentBlindness = playerScript.currentBlindness;
         //primeiro descobre a porcentagem da visão atual em relação ao maximo
-        sightPercent = (playerScript.currentBlindness / playerScript.maxBlindness);
-        sight.pointLightOuterRadius = 8 * sightPercent;
+        sightPercent = (currentBlindness / maxBlindness);
+        sight.pointLightOuterRadius = outerRadiusBase * sightPercent;
         Debug.Log(sightPercent);
     }
 }
