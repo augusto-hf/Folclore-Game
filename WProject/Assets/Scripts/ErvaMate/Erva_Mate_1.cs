d0ba9c2ccb8ae798993aa43e0c1488882ddb5a6d
Gustavo.Salaroli <74386125+Ahotchiken@users.noreply.github.com>
2021-10-14 19:46:25 -0300

    Changes?
    teste

---
+++ WProject/Assets/Scripts/ErvaMate/Erva_Mate_1.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Erva_Mate_1 : MonoBehaviour
+{
+//    [SerializeField]
+//    private bool isUsed = false;
+
+//    [SerializeField]
+//    private bool onePetal = false;
+
+//    [SerializeField]
+//    private BoxCollider2D myBoxCollider;
+
+//    private SpriteRenderer sr;
+//    private Animator anim; 
+//    private string TWO_PETAL = "TwoPetal";
+//    private string ONE_PETAL = "OnePetal";
+//    private string NO_PETAL = "NoPetal";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+} //class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
