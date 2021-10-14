5e30ec439ce569c012cc65d54916554592914e2a
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-10-14 02:26:01 -0300

    Consertando problemas no tiro
    20min

--- WProject/Assets/Scripts/Player/FireBall/flamethrower.cs
+++ WProject/Assets/Scripts/Player/FireBall/flamethrower.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class flamethrower : MonoBehaviour
 {
     public int Damage = 1;
+    private float tickTime = 1f, nextTick;
     
     // Start is called before the first frame update
   
     // Update is called once per frame
     void OnParticleCollision(GameObject collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && Time.time > nextTick)
         {
             collision.gameObject.GetComponent<Ai_Enemy_Stats>().TakeDamage(Damage);
+            nextTick = Time.time + tickTime;
         }
     }
 }
