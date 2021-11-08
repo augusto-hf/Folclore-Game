942f18b86738c4178d46762e5d682400f178f536
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-11-08 19:48:59 -0300

    Update

--- WProject/Assets/Scripts/Ost/audioFxScript.cs
+++ WProject/Assets/Scripts/Ost/audioFxScript.cs
@@ -4,35 +4,37 @@ using UnityEngine;
 
 public class audioFxScript : MonoBehaviour
 {
-    [SerializeField] AudioClip[] footstepClips;
-    [SerializeField] float FootstepsdelayBerweenClips;
+    [SerializeField] AudioClip footstepClip;
+    [SerializeField] float footstepsdelayBetweenClips;
+    public float footstepsVolume = 1.0f;
     private float footstepsTime;
-    bool canPlay;
-    public AudioSource footsteps;
+    bool footstepsHavePlayed;
+    public AudioSource footstepsSource;
 
-
+    private void Start()
+    {
+        footstepsSource = transform.Find("footsteps").GetComponent<AudioSource>();
+    }
     void Update()
     {
-        if (footstepsTime < Time.time && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+        if (footstepsTime < (Time.time * 1000) && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
         {
             PlayFootsteps();
         }
-        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        else if (footstepsHavePlayed && Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
         {
-            //footsteps.Pause();
-            
+            footstepsSource.Stop();     
         }
 
-        if (!canPlay)
-        {
-
-        }
     }
     void PlayFootsteps()
     {
-        int clipIndex = Random.Range(0, footstepClips.Length);
-        AudioClip clip = footstepClips[clipIndex];
-        footsteps.PlayOneShot(clip);
-        footstepsTime = (Time.time * 1000) + FootstepsdelayBerweenClips;
+        footstepsSource.PlayOneShot(footstepClip);
+        footstepsTime = (Time.time * 1000) + footstepsdelayBetweenClips;
+        footstepsHavePlayed = true;
+    }
+    void fxVolume()
+    {
+        footstepsSource.volume = footstepsVolume;
     }
 }
