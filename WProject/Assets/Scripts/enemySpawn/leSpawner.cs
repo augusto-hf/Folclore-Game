3b2202a97cfbf7a5199c091d15ebaf8eead42534
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-11-08 17:07:25 -0300

    Update
    palavra Floclorica e vagalumes  no menu principal

---
+++ WProject/Assets/Scripts/enemySpawn/leSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leSpawner : MonoBehaviour
+{
+    GameObject[] spawnPoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
