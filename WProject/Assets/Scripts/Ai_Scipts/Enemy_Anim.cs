5e8b2ef406f0afa9fddf4e5d4d0426ee38b535c4
DuduFcruz <73447177+DuduFcruz@users.noreply.github.com>
2021-11-21 16:14:28 -0300

    coisa

--- WProject/Assets/Scripts/Ai_Scipts/Enemy_Anim.cs
+++ WProject/Assets/Scripts/Ai_Scipts/Enemy_Anim.cs
@@ -11,39 +11,59 @@ public class Enemy_Anim : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("1");
         Zvaule = Pos.eulerAngles.z;
-        if (Zvaule >= 90 && Zvaule < 180)
+        if (Zvaule >= 90 && Zvaule < 135)
         {
-            Debug.Log("1");
             i = 0;
             x = 1;
         }
-        if (Zvaule >= 45 && Zvaule < 90)
+        if (Zvaule >= 45 && Zvaule < 65)
         {
-            Debug.Log("3");
+            i = -1;
+            x = 1;
+        }
+        if (Zvaule >= 65 && Zvaule < 90)
+        {
             i = -1;
             x = 0;
         }
-        else if (Zvaule >= 180 && Zvaule < 270)
+        else if (Zvaule >= 91 && Zvaule < 140)
         {
-            Debug.Log("2");
+          i = -1;
+          x = -1;
+
+
+        }
+        else if (Zvaule >= 180 && Zvaule < 225)
+        {
             i = 0;
             x = 1;
 
 
         }
-        else if (Zvaule >= 270 && Zvaule < 320)
+        else if (Zvaule >= 225 && Zvaule < 270)
         {
-            Debug.Log("3");
+            i = 1;
+            x = -1;
+
+
+        }
+        else if (Zvaule >= 270 && Zvaule < 315)
+        {
             i = 1;
             x = 0;
 
 
         }
-        else if (Zvaule >= 0 && Zvaule < 45 || Zvaule > 320)
+        else if (Zvaule >= 315 && Zvaule < 350)
         {
-            Debug.Log("4");
+            i = 1;
+            x = -1;
+
+
+        }
+        else if (Zvaule >= 0 && Zvaule < 45 || Zvaule > 350)
+        {
             i = 0;
             x = -1;
 
