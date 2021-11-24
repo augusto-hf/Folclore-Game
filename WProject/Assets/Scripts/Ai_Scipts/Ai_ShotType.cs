fb490a7584c6c77d19aa00010ea66f1556ef5123
DuduFcruz <73447177+DuduFcruz@users.noreply.github.com>
2021-11-24 13:40:15 -0300

    coisa 2

--- WProject/Assets/Scripts/Ai_Scipts/Ai_ShotType.cs
+++ WProject/Assets/Scripts/Ai_Scipts/Ai_ShotType.cs
@@ -129,7 +129,7 @@ public class Ai_ShotType : MonoBehaviour
         if (Vector2.Distance(_Enemy.position, Player.position) <= ai_enemy_stats.StopDistance)
             {
 
-                //_Enemy.position = Vector2.MoveTowards(_Enemy.position, _WalkBackpoint.position, ai_enemy_stats.Speed * Time.deltaTime);
+                _Enemy.position = Vector2.MoveTowards(_Enemy.position, _WalkBackpoint.position, ai_enemy_stats.Speed * Time.deltaTime);
                 if (Time.time > TimetoFire)
                 {
                     //StartCoroutine(Anim());
@@ -143,7 +143,7 @@ public class Ai_ShotType : MonoBehaviour
         if (Time.time > TimetoFire)
         {
             //StartCoroutine(Anim());
-            //ai_shootingattack.FireBallAttack();
+            ai_shootingattack.FireBallAttack();
 
             TimetoFire = Time.time + fireRate;
         }
