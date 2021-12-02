fa1db77904e43386cfefcaa23bf37f0b3ebb7758
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2021-12-02 06:32:15 -0300

    Update

--- WProject/Assets/maedouroteste.cs
+++ WProject/Assets/maedouroteste.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class maedouroteste : MonoBehaviour
 {
-    public GameObject player, troço1, troço2, troço3;
+    public GameObject player, troço1, troço2, troço4;
     public dialogueM dialogManager;
     public bool playerNear= false;
 
     void Update()
     {
-            if (playerNear)
+            if (playerNear && dialogManager.sentences.Count == 0)
             {
                 troço1.SetActive(true);
                 troço2.SetActive(true);
-                troço3.SetActive(true);
+                troço4.SetActive(false);
             }
     }
     private void OnTriggerEnter(Collider other)
